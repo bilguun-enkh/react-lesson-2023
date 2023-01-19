6f4319c6176e3ee65e36517fe4da6f9a2ac39398
Morning <morning@admins-iMac-4.local>
2023-01-19 11:10:08 +0800

    01/19/2023 day 46

---
+++ day-46/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { Outlet } from 'react-router-dom'
+import ProductList from './components/ProductList';
+import Product from './components/Product';
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import { useState } from 'react';
+
+function App() {
+  const [Wishlist, setWishlist] = useState([])
+  return (
+    <div>
+      <Header Wishlist={Wishlist} setWishlist={setWishlist} />
+      <div className="main ui text container">
+        <h1 className="ui dividing centered header">Popular Products</h1>
+        <div id="content"></div>
+        <Routes>
+          <Route path='/' element={<ProductList Wishlist={Wishlist} setWishlist={setWishlist} />} />
+          <Route path="/product/:id" element={<Product />} />
+        </Routes>
+        <Outlet />
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
