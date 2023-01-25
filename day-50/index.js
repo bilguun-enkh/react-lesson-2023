03a6deaa5787ef6ec50badcc6ca5be4f48645244
Morning <morning@admins-iMac-8.local>
2023-01-25 12:21:47 +0800

    01/25/2023 express day-50

---
+++ day-50/index.js
@@ -0,0 +1,16 @@
+console.log('Day - 50 - Express server')
+const http = require('http')
+
+const PORT = 8001
+
+const server = http.createServer((request, response) => {
+
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/plain')
+
+    response.end("Hello World\n")
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
