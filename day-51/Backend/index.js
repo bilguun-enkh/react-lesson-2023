5ddcd0ace9670eda93eb3321d0cf90331aaf4f99
Morning <morning@admins-iMac-8.local>
2023-01-26 12:34:59 +0800

    01/26/2023 day 51

---
+++ day-51/Backend/index.js
@@ -0,0 +1,52 @@
+console.log("Day - 51 : API Express JS")
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const { response } = require('express')
+const e = require('express')
+
+const app = express()
+const PORT = 8080
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+let data = [
+    {
+        'id': 1,
+        'name': 'Bilguun',
+        'major': 'Com-Sci',
+        'age': 23
+    }
+]
+
+app.get("/", (request, response) => {
+    response.send("Here I'm a brutally strong powerful Express Server!")
+})
+
+app.get("/data", (request, response) => {
+    response.json(data)
+
+})
+app.post("/data", (request, response) => {
+    const length = data.length
+    const newData = {
+        id: length + 1,
+        name: request.body.name,
+        major: request.body.major,
+    }
+    data.push(newData)
+    response.json(data)
+})
+
+app.delete("/data", (request, response) => {
+    console.log(typeof request.body)
+    const newData = data.filter(d => d.id !== request.body.id)
+    data = newData
+    response.json(data)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
