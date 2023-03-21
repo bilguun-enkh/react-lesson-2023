13b1cc9d304d9daa9a3ef762fd5e069cc7efe108
Morning <morning@admins-iMac-8.local>
2023-03-21 10:04:31 +0800

    03/21/2023 day-80 complete

--- day-80-file-upload-multer/fe/src/components/FileUpload.jsx
+++ day-80-file-upload-multer/fe/src/components/FileUpload.jsx
@@ -1,5 +1,10 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
 export default function FileUpload() {
     const URL = "http://localhost:8080/fileUpload"
+    const UPLOADSURL = "http://localhost:8080/uploads"
+    const [imagesUrl, setImages] = useState([])
     const handleUploadFile = async (event) => {
         event.preventDefault()
         console.log(event.target.image.files[0])
@@ -15,12 +20,36 @@ export default function FileUpload() {
         const FETCHED_JSON = await FETCHED_DATA.json()
         console.log(FETCHED_JSON)
     }
+
+    const fetchImage = async (event) => {
+        const options = {
+            method: "POST",
+            body: []
+        }
+        const FETCHED_DATA = await fetch(UPLOADSURL, options)
+        const FETCHED_JSON = await FETCHED_DATA.json()
+        setImages(FETCHED_JSON.data)
+        console.log(imagesUrl)
+    }
+    useEffect(() => {
+        fetchImage()
+    }, [])
     return (
         <div>
             <form method="post" encType="multipart/form-data" onSubmit={handleUploadFile} >
                 <input type="file" name="image" />
                 <button>Submit</button>
             </form>
+            <div>
+                <button onClick={fetchImage}>Fetch Image</button>
+            </div>
+            {imagesUrl && imagesUrl.map((image, index) => {
+                return (
+                    <div>
+                        <img src={image} key={index} style={{ height: "250px" }} />
+                    </div>
+                )
+            })}
         </div>
     )
 }
